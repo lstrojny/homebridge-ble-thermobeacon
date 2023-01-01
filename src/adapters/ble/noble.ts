a3f5770a345a6dbf500590bb87915b605868eaf6
Lars Strojny <lars@strojny.net>
2023-01-01 14:15:35 +0100

    Import noble dynamically to catch exception when bluetooth support is not available

--- src/adapters/ble/noble.ts
+++ src/adapters/ble/noble.ts
@@ -1,44 +1,55 @@
-import Noble, { type Peripheral } from '@abandonware/noble'
+import type Noble from '@abandonware/noble'
 import type { SensorData, ThermometerHandler } from '../../thermometer'
 import type { BlePeripheralsDiscovery } from './api'
 import { throttle } from '../../std'
+
+function newNoble(module: {
+    default: (new (args: Record<string, unknown>) => typeof Noble) | typeof Noble
+}): typeof Noble {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore Type information are foobar’ed
+    const noble = typeof module.default === 'function' ? new module.default({ extended: false }) : module.default
+    return noble
+}
 
 export const nobleDiscoverPeripherals: BlePeripheralsDiscovery = (
     handlers: ThermometerHandler[],
     sensorDataHandler: (sensorData: SensorData) => void,
     errorHandler: (error: Error) => void,
 ) => {
-    try {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore Type information are foobar’ed
-        const noble = (typeof Noble === 'function' ? new Noble({ extended: false }) : Noble) as typeof Noble
+    import('@abandonware/noble')
+        .then((module) => {
+            try {
+                const noble = newNoble(module)
 
-        noble.on('stateChange', (state: string) => {
-            if (state !== 'poweredOn') {
-                return
+                noble.on('stateChange', (state: string) => {
+                    if (state !== 'poweredOn') {
+                        return
+                    }
+
+                    noble.startScanning([], true)
+                })
+
+                noble.on(
+                    'discover',
+                    throttle(
+                        3_000,
+                        handleDiscover.bind(null, handlers, sensorDataHandler, errorHandler),
+                        (peripheral) => peripheral.uuid,
+                    ),
+                )
+            } catch (e) {
+                errorHandler(e as Error)
             }
-
-            noble.startScanning([], true)
         })
-
-        noble.on(
-            'discover',
-            throttle(
-                3_000,
-                handleDiscover.bind(null, handlers, sensorDataHandler, errorHandler),
-                (peripheral) => peripheral.uuid,
-            ),
-        )
-    } catch (e) {
-        errorHandler(e as Error)
-    }
+        .catch((e) => errorHandler(e as Error))
 }
 
 function handleDiscover(
     handlers: ThermometerHandler[],
     sensorDataHandler: (sensorData: SensorData) => void,
     errorHandler: (error: Error) => void,
-    noblePeripheral: Peripheral,
+    noblePeripheral: Noble.Peripheral,
 ) {
     const peripheral = {
         uuid: noblePeripheral.uuid,
