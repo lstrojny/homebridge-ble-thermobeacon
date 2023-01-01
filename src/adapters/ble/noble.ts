a132fc959750d0f836a60b33c73d9394901ecfcb
Lars Strojny <lars@strojny.net>
2023-01-01 14:46:51 +0100

    Use dynamic imports for noble (#22)
    Horribly enough, importing noble will fail on Linux if bluetooth is not
    supported. To work around that and have a chance to handle the exception
    gracefully, dynamically import noble but continue to import noble types
    eagerly. Using noble doesn’t get better but at least it’s all contained
    in a single module.

--- src/adapters/ble/noble.ts
+++ src/adapters/ble/noble.ts
@@ -1,37 +1,53 @@
-import Noble, { type Peripheral } from '@abandonware/noble'
+import type NobleModule from '@abandonware/noble'
 import type { SensorData, ThermometerHandler } from '../../thermometer'
 import type { BlePeripheralsDiscovery } from './api'
 import { throttle } from '../../std'
+
+type Noble = typeof NobleModule
+type Peripheral = NobleModule.Peripheral
 
 export const nobleDiscoverPeripherals: BlePeripheralsDiscovery = (
     handlers: ThermometerHandler[],
     sensorDataHandler: (sensorData: SensorData) => void,
     errorHandler: (error: Error) => void,
 ) => {
-    try {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore Type information are foobar’ed
-        const noble = (typeof Noble === 'function' ? new Noble({ extended: false }) : Noble) as typeof Noble
-
-        noble.on('stateChange', (state: string) => {
-            if (state !== 'poweredOn') {
-                return
+    import('@abandonware/noble')
+        .then((module) => {
+            try {
+                startDiscovery(newNoble(module), handlers, sensorDataHandler, errorHandler)
+            } catch (e) {
+                errorHandler(e as Error)
             }
-
-            noble.startScanning([], true)
         })
+        .catch((e) => errorHandler(e as Error))
+}
 
-        noble.on(
-            'discover',
-            throttle(
-                3_000,
-                handleDiscover.bind(null, handlers, sensorDataHandler, errorHandler),
-                (peripheral) => peripheral.uuid,
-            ),
-        )
-    } catch (e) {
-        errorHandler(e as Error)
-    }
+function newNoble(module: { default: (new (args: Record<string, unknown>) => Noble) | Noble }): Noble {
+    return typeof module.default === 'function' ? new module.default({ extended: false }) : module.default
+}
+
+function startDiscovery(
+    noble: Noble,
+    handlers: ThermometerHandler[],
+    sensorDataHandler: (sensorData: SensorData) => void,
+    errorHandler: (error: Error) => void,
+) {
+    noble.on('stateChange', (state: string) => {
+        if (state !== 'poweredOn') {
+            return
+        }
+
+        noble.startScanning([], true)
+    })
+
+    noble.on(
+        'discover',
+        throttle(
+            3_000,
+            handleDiscover.bind(null, handlers, sensorDataHandler, errorHandler),
+            (peripheral) => peripheral.uuid,
+        ),
+    )
 }
 
 function handleDiscover(
