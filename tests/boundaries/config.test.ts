815f701d9aa365eb28cb2d9ec7a5e5ff7d443b58
Lars Strojny <lars@strojny.net>
2023-01-03 19:09:24 +0100

    Fix and compact test case

--- tests/boundaries/config.test.ts
+++ tests/boundaries/config.test.ts
@@ -2,110 +2,38 @@ import { describe, expect, test } from '@jest/globals'
 import { ConfigBoundary } from '../../src/boundaries'
 import { PLATFORM_NAME } from '../../src/settings'
 
+const validAddressFormatCases: Record<string, [string, string]> = {
+    'UUID lowercase': ['e8035249ecd4495fa9bfd598dcf2fcaf', 'e8035249ecd4495fa9bfd598dcf2fcaf'],
+    'UUID lowercase dash separated': ['e8035249-ecd4-495f-a9bf-d598dcf2fcaf', 'e8035249ecd4495fa9bfd598dcf2fcaf'],
+    'UUID uppercase ': ['E8035249ECD4495FA9BFD598DCF2FCAF', 'e8035249ecd4495fa9bfd598dcf2fcaf'],
+    'UUID dash separated': ['E8035249-ECD4-495F-A9BF-D598DCF2FCAF', 'e8035249ecd4495fa9bfd598dcf2fcaf'],
+    'MAC lowercase': ['b827ebe2d683', 'b827ebe2d683'],
+    'MAC uppercase': ['B827EBE2D683', 'b827ebe2d683'],
+    'MAC lowercase colon separated': ['b8:27:eb:e2:d6:83', 'b827ebe2d683'],
+    'MAC uppercase colon separated': ['B8:27:EB:E2:D6:83', 'b827ebe2d683'],
+}
+
+const invalidAddressFormats = [1, 'some string', '00000000-00000000-00000000-00000000', undefined, null]
+
 describe('Test config boundary', () => {
-    describe('Address formats', () => {
-        test('UUID lowercase dash separated', () => {
-            expect(
-                ConfigBoundary.parse({
-                    platform: PLATFORM_NAME,
-                    devices: [{ address: 'e8035249-ecd4-495f-a9bf-d598dcf2fcaf', name: 'Device name' }],
-                }),
-            ).toEqual({
-                platform: PLATFORM_NAME,
-                debug: false,
-                devices: [{ address: 'e8035249ecd4495fa9bfd598dcf2fcaf', name: 'Device name' }],
-            })
-        })
+    describe('Valid address formats', () =>
+        Object.entries(validAddressFormatCases).forEach(([name, [input, output]]) =>
+            test(name, () =>
+                expect(
+                    ConfigBoundary.parse({
+                        platform: PLATFORM_NAME,
+                        devices: [{ address: input, name: 'Device' }],
+                    }),
+                ).toMatchObject({ devices: [{ address: output, name: 'Device' }] }),
+            ),
+        ))
 
-        test('UUID lowercase', () => {
-            expect(
-                ConfigBoundary.parse({
-                    platform: PLATFORM_NAME,
-                    devices: [{ address: 'e8035249ecd4495fa9bfd598dcf2fcaf', name: 'Device name' }],
-                }),
-            ).toEqual({
-                platform: PLATFORM_NAME,
-                debug: false,
-                devices: [{ address: 'e8035249ecd4495fa9bfd598dcf2fcaf', name: 'Device name' }],
-            })
-        })
-
-        test('UUID uppercase dash separated', () => {
-            expect(
-                ConfigBoundary.parse({
-                    platform: PLATFORM_NAME,
-                    devices: [{ address: 'E8035249-ECD4-495F-A9BF-D598DCF2FCAF', name: 'Device name' }],
-                }),
-            ).toEqual({
-                platform: PLATFORM_NAME,
-                debug: false,
-                devices: [{ address: 'e8035249ecd4495fa9bfd598dcf2fcaf', name: 'Device name' }],
-            })
-        })
-
-        test('UUID uppercase', () => {
-            expect(
-                ConfigBoundary.parse({
-                    platform: PLATFORM_NAME,
-                    devices: [{ address: 'E8035249ECD4495FA9BFD598DCF2FCAF', name: 'Device name' }],
-                }),
-            ).toEqual({
-                platform: PLATFORM_NAME,
-                debug: false,
-                devices: [{ address: 'e8035249ecd4495fa9bfd598dcf2fcaf', name: 'Device name' }],
-            })
-        })
-
-        test('MAC lowercase', () => {
-            expect(
-                ConfigBoundary.parse({
-                    platform: PLATFORM_NAME,
-                    devices: [{ address: 'b827ebe2d683', name: 'Device name' }],
-                }),
-            ).toEqual({
-                platform: PLATFORM_NAME,
-                debug: false,
-                devices: [{ address: 'b827ebe2d683', name: 'Device name' }],
-            })
-        })
-
-        test('MAC uppercase', () => {
-            expect(
-                ConfigBoundary.parse({
-                    platform: PLATFORM_NAME,
-                    devices: [{ address: 'B827EBE2D683', name: 'Device name' }],
-                }),
-            ).toEqual({
-                platform: PLATFORM_NAME,
-                debug: false,
-                devices: [{ address: 'b827ebe2d683', name: 'Device name' }],
-            })
-        })
-
-        test('MAC lowercase colon separated', () => {
-            expect(
-                ConfigBoundary.parse({
-                    platform: PLATFORM_NAME,
-                    devices: [{ address: 'b8:27:eb:e2:d6:83', name: 'Device name' }],
-                }),
-            ).toEqual({
-                platform: PLATFORM_NAME,
-                debug: false,
-                devices: [{ address: 'b827ebe2d683', name: 'Device name' }],
-            })
-        })
-
-        test('MAC uppercase colon separated', () => {
-            expect(
-                ConfigBoundary.parse({
-                    platform: PLATFORM_NAME,
-                    devices: [{ address: 'B8:27:EB:E2:D6:83', name: 'Device name' }],
-                }),
-            ).toEqual({
-                platform: PLATFORM_NAME,
-                debug: false,
-                devices: [{ address: 'b827ebe2d683', name: 'Device name' }],
-            })
-        })
+    describe('Invalid address formats', () => {
+        invalidAddressFormats.forEach((format) =>
+            test(`Invalid format ${JSON.stringify(format)}`, () =>
+                expect(() =>
+                    ConfigBoundary.parse({ platform: PLATFORM_NAME, devices: [{ address: format }] }),
+                ).toThrow()),
+        )
     })
 })
